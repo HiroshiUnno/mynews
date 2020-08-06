daa7549143881de8896518bd1fc40cf23ebc4012
海野裕 <u.hiroshi19920103@gmail.com>
2020-08-06 16:29:46 +0900

    1st commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,49 @@
+
+@extends('layouts.profile')
+@section('title', '課題の作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールの作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">Name</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">Gender</label>
+                        <div class="col-md-10">
+                        　　<input type="text" class="form-control" name="gender" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">Hobby</label>
+                        <div class="col-md-10">
+                        　　<textarea class="form-control" name="hobby" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">Introduction</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');
+     Route::get('news', 'Admin\NewsController@index');
+     Route::get('news/edit', 'Admin\NewsController@edit');
+     Route::post('news/edit', 'Admin\NewsController@update');
+     Route::get('news/delete', 'Admin\NewsController@delete');
+});
+
+//課題3
+/*
+Route::get('XXX', 'AAAContoroller@bbb');
+*/
+
+//課題4 /13-課題2,3　/14-課題3,6
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'NewsController@index');
